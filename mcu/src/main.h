73a7cd62dc99c24b3ff256e6a2fcf42d5b737562
brbownds <brbownds@g.hmc.edu>
2025-10-08 18:55:22 -0700

    here main.h

--- mcu/src/main.h
+++ mcu/src/main.h
@@ -17,11 +17,10 @@
 #define ENCODER_A PA6
 #define ENCODER_B PA8
 #define DELAY_TIM TIM2
-#define COUNT_TIM TIM6
 
-extern volatile int delta;  // time difference between encoder edges 
-extern volatile int off;    // motor off flag (1 = stopped, 0 = running)
 
 void EXTI9_5_IRQHandler(void);
+void velocity_function(void);
  
 #endif // MAIN_H
+
